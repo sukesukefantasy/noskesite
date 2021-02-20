3e5b1834f2ed21538428f12f28f5c38b4096d2d2
sukesukefantasy <58690702+sukesukefantasy@users.noreply.github.com>
2021-02-21 01:10:45 +0900

    まひろver

--- octps.js
+++ octps.js
@@ -6,18 +6,18 @@ const reset = document.getElementById("reset");
 
 // Definition EventListener Area
 
- btn.addEventListener("click" , way); 
+ btn.addEventListener("click" , way);
  reset.addEventListener("click" , del);
 
 
  function way(){
    const output = document.getElementById("output");
      rand = Math.floor(Math.random()*5); //要素数を定義
-     if (rand == 0) output.innerHTML = "れーな";
-     if (rand == 1) output.innerHTML = "タコ";
-     if (rand == 2) output.innerHTML = "nyamo";
-     if (rand == 3) output.innerHTML = "倒立";
-     if (rand == 4) output.innerHTML = "秋山";
+     if (rand == 0) output.innerHTML = "まひろ";
+     if (rand == 1) output.innerHTML = "思い出";
+     if (rand == 2) output.innerHTML = "戸田のダイナマイト";
+     if (rand == 3) output.innerHTML = "味の横綱";
+     if (rand == 4) output.innerHTML = "オクラ";
      const reset = document.getElementById("reset");
      reset.innerHTML = "RESET";
    }
